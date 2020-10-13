76ecfb54dc8be0da77d6fc91c5c9a6019665591c
Robel Seyoum <robelseyoum3@gmail.com>
2020-10-13 20:52:58 +0100

    adding to remote

---
+++ README.md
@@ -0,0 +1,4 @@
+# Meal App Flutter
+
+A Flutter application with multiple screen
+
---
+++ android/app/src/main/kotlin/com/robelseyoum/meal_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.robelseyoum.meal_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
